df7b810cb0d710b466185e820101b04ba07c1e1c
Satya Sowri Sampath Korturti <98078922+Sowri19@users.noreply.github.com>
2023-05-03 13:25:36 -0400

    creating project

---
+++ Block/Block/BlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockApp.swift
+//  Block
+//
+//  Created by Sowri on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct BlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
